bfbcdf4181b36c2469baec402b1706007ab5b7eb
Jamário Nascimento <jamariobattista@gmail.com>
2023-10-01 10:16:46 -0300

    contador funcionando

--- src/basico/Contador.java
+++ src/basico/Contador.java
@@ -11,6 +11,7 @@ import javafx.stage.Stage;
 
 public class Contador extends Application{
 	
+	private int contador = 0;
 	@Override
 	public void start(Stage primaryStage) throws Exception{
 		
@@ -18,8 +19,15 @@ public class Contador extends Application{
 		Label labelNumero = new Label("0");
 		
 		Button botaoDecremento = new Button("-");
+		botaoDecremento.setOnAction(e ->{
+			contador--;
+			labelNumero.setText(Integer.toString(contador));
+		});
 		Button botaoIncremento = new Button("+");
-		
+		botaoIncremento.setOnAction(e ->{
+			contador++;
+			labelNumero.setText(Integer.toString(contador));
+		});
 		HBox boxBotoes = new HBox();
 		boxBotoes.setAlignment(Pos.CENTER);
 		boxBotoes.setSpacing(10);
